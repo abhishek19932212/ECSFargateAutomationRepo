1efefadb1dec3f2617442104854b1f49fe39b899
abhishek19932212 <55378667+abhishek19932212@users.noreply.github.com>
2021-08-18 09:14:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World Version 4');
+  res.send('Hello World Version 5');
 });
 
 app.listen(PORT, HOST);
